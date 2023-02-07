6fb6f9954f50abee1914d6baa81091b48239e171
REdahiro <63489392+REdahiro@users.noreply.github.com>
2023-02-07 17:16:19 +0900

    Update 01.1_Milo_Case_vs_Contrl_harmony.R

--- scripts/01.1_Milo_Case_vs_Contrl_harmony.R
+++ scripts/01.1_Milo_Case_vs_Contrl_harmony.R
@@ -45,7 +45,7 @@ milo <- Milo(sce)
 milo <- buildGraph(milo, k = 30, d = 30, reduced.dim = "PCA") 
 
 ##--- Step3: Defining representative neighbourhoods on the KNN graph
-milo <- makeNhoods(milo, prop = proportion, k = 30, d = 30, refined = TRUE, reduced_dims = "PCA")    #  ALL PBMC,T/NK cells: 0.005, Mono/DC,B cells: 0.1
+milo <- makeNhoods(milo, prop = proportion, k = 30, d = 30, refined = TRUE, reduced_dims = "PCA")    #  ALL PBMC,T/NK cells: 0.05, Mono/DC,B cells: 0.1
 
 # check average neighbourhood size: over 5 x N_samples is required 
 p.NhSize = plotNhoodSizeHist(milo)
@@ -59,10 +59,8 @@ design <- data.frame(colData(milo))[,c("ID", "Status", "Age", "Sex")]
 design <- distinct(design)
 rownames(design) <- design$ID
 
-
 ##--- Step6: Computing neighbourhood connectivity---##
 milo <- calcNhoodDistance(milo, d=30, reduced.dim = "PCA")
-
 
 ##----- Step7: Testing -----##
 # The last component of the formula or last column of the model matrix are by default the test variable.
@@ -71,7 +69,6 @@ da_results <- testNhoods(milo, design = ~ Age+Sex+Status, design.df = design)
 da_results %>%
   arrange(SpatialFDR) %>%
   head() 
-
 
 milo <- buildNhoodGraph(milo)
 nh_graph_pl <- plotNhoodGraphDA(milo, da_results, layout="UMAP",alpha=0.1)     # alpha: FDR
@@ -84,7 +81,7 @@ saveRDS(milo, paste0("./Milo/",cluster,"/Milo_Case_vs_Ctrl.rds"))
 ##-----------------------------------------##
 
 #-- Assign a cell type label to each neighbourhood
-da_results <- annotateNhoods(milo, da_results, coldata_col = "l3") 
+da_results <- annotateNhoods(milo, da_results, coldata_col = "l3")    # l3: the most finest annotation
 
 p.DA_celltype = plotDAbeeswarm(da_results, group.by = "l3")
 ggsave(file = paste0("./Milo/",cluster,"/Celltype_fraction_DA.analysis_Case_vs_HC.png"), plot = p.DA_celltype, width = 7.5, height = 7.5, dpi = 300)
